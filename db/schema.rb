34649c9facb9354aebddd12611aa61aa24bc0733
Shanti Patel <shantipatel@Shantis-MacBook-Air.local>
2024-09-17 17:13:49 -0500

    Adjusted styling for ideas (incomplete)

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema[7.0].define(version: 2024_09_13_160322) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "annoucements", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.integer "created_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -47,13 +63,6 @@
     t.datetime "updated_at", null: false
   end
 
-  create_table "events", force: :cascade do |t|
-    t.string "name"
-    t.datetime "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "photos", force: :cascade do |t|
     t.text "image_data"
     t.string "title"
@@ -65,17 +74,18 @@
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "full_name"
-    t.string "uid"
-    t.string "avatar_url"
+    t.string "name"
+    t.string "image"
+    t.string "email"
+    t.string "password"
+    t.string "role"
     t.string "committee"
     t.integer "points"
-    t.integer "dues"
-    t.string "role"
+    t.string "uid"
+    t.text "token"
+    t.string "provider"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "photos", "users"
