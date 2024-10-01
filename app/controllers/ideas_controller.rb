8105fb6a6c7e1b523ea5e1d6a27ec5e654bda0ed
Shanti Patel <shantipatel@Shantis-MacBook-Air.local>
2024-10-01 16:57:34 -0500

    Added sticky note background to ideas board

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class IdeasController < ApplicationController
-  before_action :set_idea, only: %i[ show edit update destroy ]
+  before_action :set_idea, only: %i[show edit update destroy]
 
   # GET /ideas or /ideas.json
   def index
@@ -23,14 +25,10 @@ class IdeasController < ApplicationController
   def create
     @idea = Idea.new(idea_params)
 
-    respond_to do |format|
-      if @idea.save
-        format.html { redirect_to idea_url(@idea), notice: "Idea was successfully created." }
-        format.json { render :show, status: :created, location: @idea }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @idea.errors, status: :unprocessable_entity }
-      end
+    if @idea.save
+      redirect_to ideas_path, notice: 'Idea was successfully created.'
+    else
+      render :new
     end
   end
 
@@ -38,7 +36,7 @@ class IdeasController < ApplicationController
   def update
     respond_to do |format|
       if @idea.update(idea_params)
-        format.html { redirect_to idea_url(@idea), notice: "Idea was successfully updated." }
+        format.html { redirect_to idea_url(@idea), notice: 'Idea was successfully updated.' }
         format.json { render :show, status: :ok, location: @idea }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +50,20 @@ class IdeasController < ApplicationController
     @idea.destroy
 
     respond_to do |format|
-      format.html { redirect_to ideas_url, notice: "Idea was successfully destroyed." }
+      format.html { redirect_to ideas_url, notice: 'Idea was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_idea
-      @idea = Idea.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def idea_params
-      params.require(:idea).permit(:title, :description, :created_by, :created_at)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_idea
+    @idea = Idea.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def idea_params
+    params.require(:idea).permit(:title, :description, :created_by, :created_at)
+  end
 end
