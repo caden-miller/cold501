8fe5dd2dcff9b0d929e1e928b2f5b95754569f20
Erik Achtermann <erik_a@comcast.net>
2024-10-13 21:04:42 -0500

    links UI working

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,52 +3,87 @@
 class LinksController < ApplicationController
   before_action :set_link, only: %i[show edit update destroy delete]
 
-  # GET /links
   def index
-    @links = Link.all
+    @links = Link.order(:id)
   end
 
   # GET /links/new
   def new
+    puts "Inside New link"
     @link = Link.new
+    puts "Leaving New link"
   end
 
-  # POST /links
-  def create
-    @link = Link.new(link_params)
+    # POST /links
+    def create
+      puts "Inside Create link"
+      @link = Link.new(link_params)
+      # @link = link.new(link_params)
+      # @link.user = current_user
+    
+      if @link.save
+        puts "link Created"
+        respond_to do |format|
+          
+          format.html { redirect_to links_path, notice: "link Created" }
+          format.turbo_stream
+        end
+      else
+        puts "link Not Created"
+        render :new, status: :unprocessable_entity
+      end
+    end
 
-    if @link.save
-      redirect_to links_path, notice: 'Link was successfully created.'
-    else
-      render :new
+  def show; end
+
+  def edit
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.replace(dom_id(@link), partial: 'links/form', locals: { link: @link }) }
+      format.html
     end
   end
 
-  # GET /links/:id/edit
-  def edit
-    @link = Link.find(params[:id])
-  end
-
-  # PATCH/PUT /links/:id
   def update
     if @link.update(link_params)
-      redirect_to links_path, notice: 'Link was successfully updated.'
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to links_path, notice: 'link was successfully updated.' }
+      end
     else
-      render :edit
+      respond_to do |format|
+        format.html { render :new, status: :unprocessable_entity }
+        format.turbo_stream {
+          render turbo_stream: turbo_stream.replace(
+            dom_id(@link),
+            partial: 'links/form',
+            locals: { link: @link }
+          ), status: :unprocessable_entity
+        }
+      end
     end
   end
 
   def delete; end
 
-  # DELETE /links/:id
   def destroy
-    @link = Link.find(params[:id])
-    if @link.destroy
-      flash[:success] = 'Link was successfully deleted.'
-    else
-      flash[:error] = 'Link could not be deleted.'
+    @link.destroy
+    respond_to do |format|
+      format.html { redirect_to links_path, notice: 'link was successfully deleted.' }
+      format.turbo_stream
     end
+  end
+
+  def leaderboard
+    @links = link.order(:id)
+  end
+
+  def reset_points
+    # link = link.first  # Or any specific link
+    puts "trying to update"
+    Link.update_all(points: 0)
+    puts "updated"
     redirect_to links_path
+    # redirect_to links_path
   end
 
   private
@@ -58,6 +93,9 @@ class LinksController < ApplicationController
   end
 
   def link_params
-    params.require(:link).permit(:title, :link)
+    params.require(:link).permit(
+      :title,
+      :link,
+    )
   end
 end
