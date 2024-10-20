e62cbee2f81b1b53316ccf1d1f44f0f5b25b8311
caden-miller <cadenmiller40@gmail.com>
2024-10-20 17:54:48 -0500

    followed customer instructions for home page carousels.

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -1,37 +1,34 @@
-// app/javascript/controllers/carousel_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["slide"]
+  static targets = ["slide", "description"]
+  static values = { descriptions: Array }
 
   initialize() {
     this.currentIndex = 0
   }
 
   connect() {
-    console.log("Connected!")
     this.showCurrentSlide()
   }
 
   next() {
-    this.currentIndex++
-    if (this.currentIndex >= this.slideTargets.length) {
-      this.currentIndex = 0
-    }
+    this.currentIndex = (this.currentIndex + 1) % this.slideTargets.length
     this.showCurrentSlide()
   }
 
   previous() {
-    this.currentIndex--
-    if (this.currentIndex < 0) {
-      this.currentIndex = this.slideTargets.length - 1
-    }
+    this.currentIndex = (this.currentIndex - 1 + this.slideTargets.length) % this.slideTargets.length
     this.showCurrentSlide()
   }
 
   showCurrentSlide() {
+    // Update slides
     this.slideTargets.forEach((element, index) => {
       element.classList.toggle("hidden", index !== this.currentIndex)
     })
+
+    // Update description
+    this.descriptionTarget.textContent = this.descriptionsValue[this.currentIndex]
   }
 }
